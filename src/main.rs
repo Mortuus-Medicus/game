f3161cc8b6a14a67294bd9f27c6046fd5014f590
Mortuus <mortuusmedicus@gmail.com>
2022-07-22 14:14:07 +0200

    Okay, now it really works?

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ async fn main() {
 
         draw_line(40.0, 40.0, 100.0, 200.0, 15.0, RED);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, PURPLE);
         
         draw_text("IT WORKS!", 20.0, 20.0, 30.0, MAGENTA);
         draw_text("Wee Woo!", 20.0, 50.0, 30.0, PINK);
