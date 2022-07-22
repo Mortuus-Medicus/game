083d2d7a58b38ef7e62033054c70cb9829626317
Mortuus-Medicus <mortuusmedicus@gmail.com>
2022-07-23 00:31:19 +0200

    Tested mouse input and image drawing

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,29 @@ async fn main() {
     loop {
         clear_background(DARKGRAY);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, RED);
+        let texture = Texture2D::from_file_with_format(
+            include_bytes!("../assets/CrystalSagaProfileImage.png"),
+            None,
+        );
+
+        draw_texture(texture, 50.0, 50.0, WHITE);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
         draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, BLUE);
-        
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, MAGENTA);
-        draw_text("Wee Woo!", 20.0, 50.0, 30.0, PINK);
 
+        draw_text("IT WORKS!", 20.0, 20.0, 30.0, MAGENTA);
+        draw_text(&get_fps().to_string(), 20.0, 50.0, 30.0, PINK);
+
+        let temp_mouse_position = mouse_position();
+
+        if is_mouse_button_down(MouseButton::Left) == true {
+            draw_circle(temp_mouse_position.0, temp_mouse_position.1, 5.0, RED);
+        } else {
+            draw_circle(temp_mouse_position.0, temp_mouse_position.1, 5.0, WHITE);
+        }
+
+        if is_key_pressed(KeyCode::Escape) {
+            break;
+        }
         next_frame().await
     }
 }
