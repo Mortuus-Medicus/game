ebee374ea6346a146af3b4e0ff5515d6a27d3067
Mortuus <mortuusmedicus@gmail.com>
2022-07-22 14:03:44 +0200

    Bleep Bloop

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,8 @@ async fn main() {
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
         draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
         
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
-        draw_text("Wee Woo!", 20.0, 50.0, 30.0, DARKGRAY);
+        draw_text("IT WORKS!", 20.0, 20.0, 30.0, MAGENTA);
+        draw_text("Wee Woo!", 20.0, 50.0, 30.0, PINK);
 
         next_frame().await
     }
