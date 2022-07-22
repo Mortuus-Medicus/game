0c247543150222065172103c82ea8a29b0a60edc
Mortuus <mortuusmedicus@gmail.com>
2022-07-22 14:06:33 +0200

    I think git works now

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ async fn main() {
     loop {
         clear_background(BLACK);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, RED);
         draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
         draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
         
